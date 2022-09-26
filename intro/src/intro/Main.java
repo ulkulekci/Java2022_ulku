fe50e43bdc9ed56868072f191b803ef6bf773960
ulkulekci <ulkusulu07@hotmail.com>
2022-09-27 02:17:21 +0300

    1.hafta derste yapılanlar

---
+++ intro/src/intro/Main.java
@@ -0,0 +1,39 @@
+package intro;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("HELLO WORLD!");
+		String ortaMetin = "İlginizi çekebilir";
+		String altMetin = "Vade süresi";
+		System.out.println(ortaMetin);
+		
+		int vade = 12;
+		
+		double dolarDun = 18.14;
+		double dolarBugun = 18.10;
+		
+		boolean dolarDustuMu = true;
+		
+		String okYonu = "";
+		
+		if (dolarBugun < dolarDun) {
+			okYonu = "down.svg";
+			System.out.println(okYonu);
+		} else if (dolarBugun > dolarDun) {
+			okYonu = "up.svg";
+			System.out.println(okYonu);
+		} else {
+			okYonu = "equal.svg";
+			System.out.println(okYonu);
+		}
+		
+		String[] krediler = { "Hızlı Kredi", "Maaşınız Halkbanktan", "Mutlu Emekli" };
+		for (int i = 0; i < krediler.length; i++) {
+			System.out.println(krediler[i]);
+		}
+		
+	}
+	
+}
